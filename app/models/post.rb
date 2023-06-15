311fd1b93d0657078b59426ac738c3350da0b136
Luchy-B <okparabon@gmail.com>
2023-06-15 16:52:37 +0100

    Add Associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+  belongs_to: :author, class_name: "User"
+  has_many :comments
+  has_many :likes
+  after_save: :update_post_counter
+
+  def recent_five_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_post_counter
+      author.increment!(:post_counter)
+  end
 end

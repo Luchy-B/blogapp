a93c268f0bb40a43864aee25eeceb14a79ee2418
Luchy-B <okparabon@gmail.com>
2023-06-16 15:41:27 +0100

    Add test for blog App

--- db/schema.rb
+++ db/schema.rb
@@ -46,8 +46,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_15_085141) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.text "bio"
     t.string "photo"
+    t.text "bio"
     t.integer "post_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

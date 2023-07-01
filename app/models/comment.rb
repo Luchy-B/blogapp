6370fb73483a336d703fb03751d46e9c1ede7b24
Luchy-B <okparabon@gmail.com>
2023-07-01 11:17:36 +0100

    Add devise to project

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,11 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
-  after_save :update_comment_counter
+
+  after_create :update_comment_counter
 
   private
-
+  
   def update_comment_counter
     post.increment!(:comments_counter)
   end
